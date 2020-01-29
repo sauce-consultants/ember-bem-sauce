d6514a6ab57c9ff3d7ea59da225bf27f497c492b
Paweł <jembezmamy@users.noreply.github.com>
2020-01-29 11:14:43 +0100

    bem helper: allow to skip element

--- tests/integration/helpers/bem-test.js
+++ tests/integration/helpers/bem-test.js
@@ -68,6 +68,18 @@ test('test helper returns correct modifier classes with additional custom modifi
   assert.equal(this.$().text().trim(), 'component__element component__element--active component__element--primary component__element--foo');
 });
 
+test('test helper returns correct modifier classes without even if no element is provided', function(assert) {
+
+  this.set('b', 'component');
+  this.set('m', ['active', 'primary']);
+  this.set('boop', false);
+  this.set('bar', true);
+
+  this.render(hbs `{{bem b m boop=boop foo=bar}}`);
+
+  assert.equal(this.$().text().trim(), 'component component--active component--primary component--foo');
+});
+
 test('test dummy component', function(assert) {
   this.set('disabled', false);
 
